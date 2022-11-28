9d25e961bfd8f867ea6ef0064a05e452e6e4575f
Manuel Kohn <mk@vg-mediastudio.de>
2022-11-28 09:01:52 +0100

    add second stopmotion clip for mobile

--- src/lib/sections/Bikewash.svelte
+++ src/lib/sections/Bikewash.svelte
@@ -18,9 +18,13 @@
 
 <section class="bikewash">
     <div class="row">
-        <video class="background" use:initBackgroundVideo muted playsinline loop preload="metadata">
-            <source src="src/assets/images/bikewash/bikesoap-stopmotion.webm" type="video/webm">
-            <source src="src/assets/images/bikewash/bikesoap-stopmotion.mp4" type="video/mp4">
+        <video class="background desktop" use:initBackgroundVideo muted playsinline loop preload="metadata">
+            <source src="src/assets/images/bikewash/bikesoap-stopmotion-landscape.webm" type="video/webm">
+            <source src="src/assets/images/bikewash/bikesoap-stopmotion-landscape.mp4" type="video/mp4">
+        </video>
+        <video class="background mobile" use:initBackgroundVideo muted playsinline loop preload="metadata">
+            <source src="src/assets/images/bikewash/bikesoap-stopmotion-portrait.webm" type="video/webm">
+            <source src="../../assets/images/bikewash/bikesoap-stopmotion-portrait.mp4" type="video/mp4">
         </video>
         <div class="container">
             <div class="span-12">
@@ -48,31 +52,49 @@
         right: 0;
         width: 100%;
         height: 100%;
+        max-height: 100vh;
+        max-width: 100vw;
         object-fit: cover;
 
         z-index: -1;
-      }
-      .row {
-        @include media-breakpoint-down(md) {
-          &:after {
-            content: ' ';
-            position: absolute;
-            top: 0;
-            left: 0;
-            bottom: 0;
-            right: 0;
 
-            background: rgba(white, 0.2);
-
-            z-index: 0;
+        &.mobile {
+          display: block;
+          @include media-breakpoint-up(lg) {
+            display: none;
           }
         }
+        &.desktop {
+          display: none;
+          @include media-breakpoint-up(lg) {
+            display: block;
+          }
+        }
+      }
+      .row {
+        @include media-breakpoint-up(lg) {
+          height: 100vh;
+        }
+        //@include media-breakpoint-down(md) {
+        //  &:after {
+        //    content: ' ';
+        //    position: absolute;
+        //    top: 0;
+        //    left: 0;
+        //    bottom: 0;
+        //    right: 0;
+        //
+        //    background: rgba(white, 0.2);
+        //
+        //    z-index: 0;
+        //  }
+        //}
       }
       .span-12 {
         z-index: 2;
         margin: $section-margin 0;
-        @include media-breakpoint-up(lg) {
-          height: 100vh;
+        @include media-breakpoint-down(md) {
+          padding: 0 0 calc($section-margin * 1.75) 0;
         }
 
         .text-container {
